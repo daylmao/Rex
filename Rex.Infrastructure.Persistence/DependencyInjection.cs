083ab69c2a97392e305d413f1e4ecdc69dd97314
Dayron <Dayronbp06@gmail.com>
2025-08-31 20:29:05 -0400

    feat: add di class

--- Rex.Infrastructure.Persistence/DependencyInjection.cs
+++ Rex.Infrastructure.Persistence/DependencyInjection.cs
@@ -1,5 +1,22 @@
-﻿namespace Rex.Infrastructure.Persistence;
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Rex.Infrastructure.Persistence.Context;
 
-public class Class1
+namespace Rex.Infrastructure.Persistence;
+
+public static class DependencyInjection
 {
+    public static void AddPersistence(this IServiceCollection services, IConfiguration configuration)
+    {
+        #region DbContext
+
+        services.AddDbContext<RexContext>(postgres =>
+        {
+            postgres.UseNpgsql(configuration.GetConnectionString("Rex"), 
+                m => m.MigrationsAssembly("Rex.Infrastructure.Persistence"));
+        });
+
+        #endregion
+    }
 }
