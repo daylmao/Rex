ee787d1d1190d0dd4d8b2cd72d7630551696959f
Dayron <Dayronbp06@gmail.com>
2025-09-12 14:51:49 -0400

    feat: add confirm account endpoint

--- Rex.Presentation.Api/Controllers/UsersController.cs
+++ Rex.Presentation.Api/Controllers/UsersController.cs
@@ -1,6 +1,7 @@
 using Asp.Versioning;
 using MediatR;
 using Microsoft.AspNetCore.Mvc;
+using Rex.Application.Modules.User.Commands.ConfirmAccount;
 using Rex.Application.Modules.User.Commands.RegisterUser;
 
 namespace Rex.Presentation.Api.Controllers;
@@ -22,6 +23,19 @@ public class UsersController(
         {
             return Ok(result.Value);
         }
+        return BadRequest(result.Error);
+    }
+
+    [HttpPost("confirm-account")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    public async Task<IActionResult> ConfirmAccountAsync(ConfirmAccountCommand command,
+        CancellationToken cancellationToken)
+    {
+        var result = await mediator.Send(command, cancellationToken);
+        if (result.IsSuccess)
+            return Ok(result.Value);
+        
         return BadRequest(result.Error);
     }
 }
