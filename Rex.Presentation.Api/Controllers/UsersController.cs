d7e10e7cb858b2b8d1fc12992f0f24f2dab4538f
Dayron <Dayronbp06@gmail.com>
2025-09-12 21:28:05 -0400

    feat: add resend code flow

--- Rex.Presentation.Api/Controllers/UsersController.cs
+++ Rex.Presentation.Api/Controllers/UsersController.cs
@@ -2,7 +2,9 @@ using Asp.Versioning;
 using MediatR;
 using Microsoft.AspNetCore.Mvc;
 using Rex.Application.Modules.User.Commands.ConfirmAccount;
+using Rex.Application.Modules.User.Commands.Login;
 using Rex.Application.Modules.User.Commands.RegisterUser;
+using Rex.Application.Modules.User.Commands.ResendCode;
 
 namespace Rex.Presentation.Api.Controllers;
 
@@ -36,6 +38,30 @@ public class UsersController(
         if (result.IsSuccess)
             return Ok(result.Value);
         
+        return BadRequest(result.Error);
+    }
+
+    [HttpPost("resend-code")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    public async Task<IActionResult> ResendCodeAsync(ResendCodeCommand command, CancellationToken cancellationToken)
+    {
+        var result = await mediator.Send(command, cancellationToken);
+        if (result.IsSuccess)
+            return Ok(result.Value);
+        return BadRequest(result.Error);
+    }
+
+    [HttpPost("login")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    public async Task<IActionResult> LoginAsync(LoginCommand command, CancellationToken cancellationToken)
+    {
+        var result = await mediator.Send(command, cancellationToken);
+        if (result.IsSuccess)
+        {
+            return Ok(result.Value);
+        }
         return BadRequest(result.Error);
     }
 }
