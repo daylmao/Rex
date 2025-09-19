2b924b26c6d788e1deb6284f94b6230fcf376996
Dayron Bello <dayronbp06@gmail.com>
2025-09-19 17:46:10 -0400

    feat: add GetUserDetails query

---
+++ Rex.Application/Modules/User/Queries/GetUserDetails/GetUserDetailsByIdQueryHandler.cs
@@ -0,0 +1,49 @@
+using Microsoft.Extensions.Caching.Distributed;
+using Microsoft.Extensions.Logging;
+using Rex.Application.Abstractions.Messages;
+using Rex.Application.DTOs;
+using Rex.Application.Interfaces.Repository;
+using Rex.Application.Utilities;
+
+namespace Rex.Application.Modules.User.Queries.GetUserDetails;
+
+public class GetUserDetailsByIdQueryHandler(
+    ILogger<GetUserDetailsByIdQueryHandler> logger,
+    IUserRepository userRepository,
+    IDistributedCache cache
+    ): IQueryHandler<GetUserDetailsByIdQuery, UserProfileDto>
+{
+    public async Task<ResultT<UserProfileDto>> Handle(GetUserDetailsByIdQuery request, CancellationToken cancellationToken)
+    {
+        if (request is null)
+        {
+            logger.LogWarning("GetUserDetailsByIdQuery request is null.");
+            return ResultT<UserProfileDto>.Failure(Error.Failure("400", "Request cannot be null."));
+        }
+    
+        var userCache = await cache.GetOrCreateAsync(
+            $"get-user-details-{request.UserId.ToString().ToLower()}",
+            async() => await userRepository.GetUserDetailsAsync(request.UserId, cancellationToken), logger ,cancellationToken:cancellationToken);
+
+        UserProfileDto userDto = new(
+            userCache.FirstName,
+            userCache.LastName,
+            userCache.Email,
+            userCache.UserName,
+            userCache.ProfilePhoto,
+            userCache.CoverPhoto,
+            userCache.Birthday,
+            userCache.CreatedAt,
+            userCache.Biography,
+            userCache.Gender,
+            userCache.LastLoginAt,
+            userCache.UserGroups?.Count ?? 0,
+            userCache.Reactions?.Count ?? 0,
+            userCache.UserChallenges?.Count ?? 0
+        );
+    
+        logger.LogInformation("User details retrieved successfully for UserId {UserId}.", request.UserId);
+        return ResultT<UserProfileDto>.Success(userDto);
+    }
+
+}
