fee2c56d1c8d58e231bbc25157dc35b0d4eed3bb
Dayron Bello <dayronbp06@gmail.com>
2025-09-20 21:25:49 -0400

    feat(migration): add file properties to Group model
    Add database migration to include file-related properties in Group table

---
+++ Rex.Infrastructure.Persistence/Migrations/20250920213211_AddFilesPropertiesInGroupModel.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Rex.Infrastructure.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddFilesPropertiesInGroupModel : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "CoverPhoto",
+                table: "Group",
+                type: "character varying(255)",
+                maxLength: 255,
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "ProfilePhoto",
+                table: "Group",
+                type: "character varying(255)",
+                maxLength: 255,
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CoverPhoto",
+                table: "Group");
+
+            migrationBuilder.DropColumn(
+                name: "ProfilePhoto",
+                table: "Group");
+        }
+    }
+}
