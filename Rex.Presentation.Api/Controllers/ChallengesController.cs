ce26785b0d33b900e395033bcc708eaadf2b2f3d
Dayron <dayronbp06@gmail.com>
2025-09-24 22:56:41 -0400

    feat: add challenges controller

--- Rex.Presentation.Api/Controllers/ChallengesController.cs
+++ Rex.Presentation.Api/Controllers/ChallengesController.cs
@@ -1,8 +1,11 @@
 using Asp.Versioning;
 using MediatR;
 using Microsoft.AspNetCore.Mvc;
+using Rex.Application.Modules.Challenges.Commands.JoinChallenge;
+using Rex.Application.Modules.Challenges.Commands.UpdateChallenge;
 using Rex.Application.Modules.Challenges.CreateChallenge;
 using Rex.Application.Modules.Challenges.Queries.GetChallengesByStatus;
+using Rex.Application.Modules.Challenges.Queries.GetChallengesByUser;
 using Rex.Enum;
 
 namespace Rex.Presentation.Api.Controllers;
@@ -46,6 +49,68 @@ public class ChallengesController(IMediator mediator) : ControllerBase
         }
 
         return result.Error.Code switch
+        {
+            "404" => NotFound(result.Error),
+            _ => BadRequest(result.Error),
+        };
+    }
+    
+    [HttpPost("{challengeId}/join")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    public async Task<IActionResult> JoinChallengeAsync([FromRoute] Guid challengeId, [FromRoute] Guid userId 
+        ,CancellationToken cancellation)
+    {
+        var result = await mediator.Send(new JoinChallengeCommand(challengeId, userId), cancellation);
+
+        if (result.IsSuccess)
+        {
+            return Ok(result.Value);
+        }
+
+        return result.Error!.Code switch
+        {
+            "404" => NotFound(result.Error),
+            _ => BadRequest(result.Error),
+        };
+    }
+    
+    [HttpPut("{challengeId}")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    public async Task<IActionResult> UpdateChallengeAsync([FromBody] UpdateChallengeCommand command, CancellationToken cancellation)
+    {
+        var result = await mediator.Send(command, cancellation);
+
+        if (result.IsSuccess)
+        {
+            return Ok(result.Value);
+        }
+
+        return result.Error!.Code switch
+        {
+            "404" => NotFound(result.Error),
+            _ => BadRequest(result.Error),
+        };
+    }
+    
+    [HttpGet("user/{userId}")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    public async Task<IActionResult> GetChallengesByUserAsync([FromRoute] Guid userId, [FromQuery] UserChallengeStatus status,
+        [FromQuery] int pageNumber, [FromQuery] int pageSize, CancellationToken cancellationToken)
+    {
+        var result = await mediator.Send(new GetChallengesByUserQuery(userId, status, pageNumber, pageSize), cancellationToken);
+
+        if (result.IsSuccess)
+        {
+            return Ok(result.Value);
+        }
+
+        return result.Error!.Code switch
         {
             "404" => NotFound(result.Error),
             _ => BadRequest(result.Error),
