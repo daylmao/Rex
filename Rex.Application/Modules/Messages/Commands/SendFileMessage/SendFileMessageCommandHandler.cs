2efe945f984cf641aaf2ee993ac57749d9b54f9a
Dayron <dayronbp06@gmail.com>
2025-10-07 20:10:12 -0400

    feat: add file message sending capability
    Introduces SendFileMessageCommand and SendFileMessageCommandHandler to support
    file attachments in chat messages.

---
+++ Rex.Application/Modules/Messages/Commands/SendFileMessage/SendFileMessageCommandHandler.cs
@@ -0,0 +1,22 @@
+using Microsoft.Extensions.Logging;
+using Rex.Application.Abstractions.Messages;
+using Rex.Application.DTOs;
+using Rex.Application.Interfaces;
+using Rex.Application.Utilities;
+
+namespace Rex.Application.Modules.Messages.Commands.SendFileMessage;
+
+public class SendFileMessageCommandHandler(
+    ILogger<SendFileMessageCommandHandler> logger,
+    IMessageService messageService
+    ): ICommandHandler<SendFileMessageCommand, MessageDto>
+{
+    public async Task<ResultT<MessageDto>> Handle(SendFileMessageCommand request, CancellationToken cancellationToken)
+    {
+        if (request is null)
+            return ResultT<MessageDto>.Failure(Error.Failure("400", "Invalid request. Please try again."));
+        
+        return await messageService.SendMessageAsync(request.ChatId, request.UserId, request.Message, request.Files, cancellationToken);
+        
+    }
+}
