3e614028c6ec22364907e7e9b9dbc622a8f9abe6
Dayron <dayronbp06@gmail.com>
2025-10-11 19:36:32 -0400

    feat: add group member role management
    Implements UpdateGroupRoleMember command to enable administrators to manage
    member permissions and roles within group structures

---
+++ Rex.Application/Modules/Groups/Commands/UpdateGroupRoleMember/UpdateGroupRoleMemberCommand.cs
@@ -0,0 +1,12 @@
+using System.Windows.Input;
+using Rex.Application.Abstractions.Messages;
+using Rex.Application.DTOs.JWT;
+using Rex.Enum;
+
+namespace Rex.Application.Modules.Groups.Commands.UpdateGroupRoleMember;
+
+public record UpdateGroupRoleMemberCommand(
+    Guid UserId,
+    Guid GroupId,
+    GroupRole Role
+    ): ICommand<ResponseDto>;
