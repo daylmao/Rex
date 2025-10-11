53c5468c1c442ab73a7299893c9565719b17c932
Dayron <dayronbp06@gmail.com>
2025-10-11 19:35:39 -0400

    refactor: rename message queries and remove legacy reaction commands
    Renames GetMessagesByUserId to GetMessagesByChatId for clearer semantics and
    removes ToggleLikeCommand in preparation for enhanced reaction system

--- Rex.Application/Modules/Messages/Queries/GetMessagesByChatId/GetMessagesByChatIdQuery.cs
+++ Rex.Application/Modules/Messages/Queries/GetMessagesByChatId/GetMessagesByChatIdQuery.cs
@@ -2,7 +2,7 @@ using Rex.Application.Abstractions.Messages;
 using Rex.Application.DTOs.Message;
 using Rex.Application.Pagination;
 
-namespace Rex.Application.Modules.Messages.Queries.GetMessagesByUserId;
+namespace Rex.Application.Modules.Messages.Queries.GetMessagesByChatId;
 
 public record GetMessagesByUserIdQuery(
     Guid ChatId,
